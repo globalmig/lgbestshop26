f66608fe5565a38b4d657263e7f9d17919457e7f
mig_hyebin <hyebinkimdesign@gmail.com>
2026-06-09 14:31:45 +0900

    fix: 히어로 이미지 unoptimized 추가 (Cloudflare Workers R2 404 해결)

--- components/HeroSlider.tsx
+++ components/HeroSlider.tsx
@@ -69,6 +69,7 @@ export default function HeroSlider({ initialSlides = [] }: { initialSlides?: Sli
             fill
             priority={i === 0}
             sizes="100vw"
+            unoptimized
             className="object-cover object-top"
           />
         </div>
